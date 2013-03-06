9784f0233a186103d08be7aa6316066ac65442eb
Alex Elliott <alex@alex-elliott.co.uk>
2013-03-06 14:14:32 +0000

    Added FileStatus enumeration

--- src/gpfile.hpp
+++ src/gpfile.hpp
@@ -10,6 +10,22 @@ class GPFile : public QObject
     Q_OBJECT
 
 public:
+    /*!
+     * \brief Enumeration covering the possible states of a file in the system
+     */
+    enum FileStatus
+    {
+        //! Normal, no changes
+        Normal,
+        //! File has been modified
+        Modified,
+        //! File has been deleted / file not found
+        Deleted,
+
+        //! Error, unknown issue
+        Error
+    };
+
     GPFile(const QString &filePath = QString(), QObject *parent = 0);
 
     QString path() const;
